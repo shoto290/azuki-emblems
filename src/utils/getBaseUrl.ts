a1e03b7cdb1b8cbb05a0d4948d5cb3ae5ed7759a
Shoto <38501963+shoto290@users.noreply.github.com>
2024-09-24 09:41:16 +0200

    Remove comments

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,15 +1,11 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") {
-    // Côté client
     return window.location.origin;
   }
 
-  // Côté serveur
   if (process.env.VERCEL_URL) {
-    // Déploiement Vercel
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  // Environnement de développement ou autre hébergeur
   return `http://localhost:${process.env.PORT || 3000}`;
 }
