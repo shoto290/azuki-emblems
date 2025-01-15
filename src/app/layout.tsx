90902556c622abd36c9aa0b2d29899cce2e61bdd
Shoto <38501963+shoto290@users.noreply.github.com>
2025-01-15 21:05:46 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import Provider from "./provider";
 import { Metadata } from "next";
 import { WarningModal } from "./components/WarningModal";
+import { Button } from "@/lib/ui/components/ui/button";
+import Link from "next/link";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,17 +28,23 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
       </head>
+
       <body
-        className={cn(
-          "px-4 min-h-screen bg-background font-sans antialiased flex justify-center",
-          fontSans.variable
-        )}
+        className={cn("bg-background font-sans antialiased", fontSans.variable)}
       >
-        <Provider>
-          <div className="w-full max-w-[1400px] mt-4">{children}</div>
-        </Provider>
-        <Analytics />
-        <WarningModal />
+        <div className="z-50 fixed top-0 left-0 right-0 w-full h-14 bg-black flex items-center justify-center gap-2">
+          Garden Emblem is leveling up
+          <Link href="https://www.kiroshi.app/gallery/azuki">
+            <Button>Go to Kiroshi.app</Button>
+          </Link>
+        </div>
+        <div className={cn("px-4 pt-10 min-h-[80dvh] flex justify-center")}>
+          <Provider>
+            <div className="w-full max-w-[1400px] mt-4">{children}</div>
+          </Provider>
+          <Analytics />
+          <WarningModal />
+        </div>
       </body>
     </html>
   );
